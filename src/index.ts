b20a46c3e11e9875bf7dbea5f13204e4a75bb4a4
Alif Robby Alcafi <43719900+MbuuL@users.noreply.github.com>
2026-02-24 22:56:11 +0000

    add helmet and cors configure helmet and cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import "dotenv/config";
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
