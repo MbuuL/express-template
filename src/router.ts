617ffa049e00ca518e220d34b699f3fbb9908ead
Alif Robby Alcafi <43719900+MbuuL@users.noreply.github.com>
2026-02-26 19:03:31 +0000

    add simple check-auth route

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@
 import { Router, type Router as RouterType } from "express";
 import { getHealth, getVersion } from "./controllers/misc";
 import { register, login } from "./controllers/auth";
+import { authMiddleware } from "./middlewares/auth";
 
 const router: RouterType = Router();
 
@@ -12,5 +13,8 @@ router.get("/health", getHealth);
 router.get("/version", getVersion);
 router.post("/auth/register", register);
 router.post("/auth/login", login);
+router.get("/check-auth", authMiddleware, (req, res) => {
+  res.json({ message: "Authenticated", user: req.user });
+});
 
 export default router;
