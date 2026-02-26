7c525e56ff32d1c2899a1458800af0bcb729b1cb
Alif Robby Alcafi <43719900+MbuuL@users.noreply.github.com>
2026-02-26 19:03:20 +0000

    add middleware to check token

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Authorization header missing" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Token missing" });
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+  req.user = decoded;
+  next();
+};
